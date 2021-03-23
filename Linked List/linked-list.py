390075b5c326ae3083279a5df03f4836d1459419
drhiromu <drhiromu@gmail.com>
2021-03-23 18:01:34 +0800

    [add] linked list

---
+++ Linked List/linked-list.py
@@ -0,0 +1,60 @@
+class ListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        return
+
+class SingleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        return
+    
+    def add_node(self, item):
+        if not isinstance(item, ListNode):
+            item = ListNode(item)
+
+        if self.head is None:
+            self.head = item
+        else:
+            self.tail.next = item
+        
+        self.tail = item
+        return
+    
+    def show_all_elements(self):
+        print('Elements in the linked list:')
+        currentNode = self.head
+        while currentNode:   
+            print(currentNode.data)
+            currentNode = currentNode.next
+
+            
+    
+    def reverse(self):
+        print('Reverse linked list')
+        currentNode = None
+        prevNode = None
+        nextNode = self.head
+        self.tail = self.head
+        while nextNode is not None:            
+            currentNode = nextNode               
+            nextNode = currentNode.next
+            currentNode.next = prevNode
+            prevNode = currentNode
+        self.head = currentNode      
+        
+        return
+
+
+
+node1 = ListNode(24)
+list1 = SingleLinkedList()
+list1.add_node(node1)
+list1.add_node(33)
+list1.add_node(55)
+
+list1.show_all_elements()
+
+list1.reverse()
+list1.show_all_elements()
